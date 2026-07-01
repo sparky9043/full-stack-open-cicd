85fb78fa9c1f0736b3feaa68565a04c793b6763f
Se Joon Park <sparky9043@gmail.com>
2026-07-01 15:09:09 +0900

    add test for clicking a pokmeon and displaying proper info

--- e2e-tests/example.spec.js
+++ e2e-tests/example.spec.js
@@ -7,3 +7,12 @@ describe('Pokedex', () => {
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
 })
+
+describe('Ivysaur', () => {
+  test('Ivysaur page opens and contains proper content', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('Ivysaur').click()
+    await expect(page.getByText('Speed')).toBeVisible()
+    await expect(page.getByText('Chlorophyll')).toBeVisible()
+  })
+});
